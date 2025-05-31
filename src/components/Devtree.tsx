8f9544343c976d64330e73c119163e85cd6abb01
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-05-31 15:13:06 -0400

    Add tabs & heroicons & Devtree component setup

--- src/components/Devtree.tsx
+++ src/components/Devtree.tsx
@@ -1,14 +1,38 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+
+import NavigationTabs from "./NavigationTabs";
+import type { User } from "../types";
 import Header from "./Header";
 
-const Devtree = () => {
+type DevTreeProps = {
+  data: User;
+};
+
+export default function DevTree({ data }: DevTreeProps) {
   return (
     <>
       <Header />
-      Devtree
-      <Outlet />
+
+      <div className="bg-gray-100  min-h-screen py-10">
+        <main className="mx-auto max-w-5xl p-10 md:p-0">
+          <NavigationTabs />
+
+          <div className="flex justify-end">Link a vista publica</div>
+
+          <div className="flex flex-col md:flex-row gap-10 mt-10">
+            <div className="flex-1 ">
+              <Outlet />
+            </div>
+            <div className="w-full md:w-96 bg-slate-800 px-5 py-10 space-y-6">
+              <p className="text-4xl text-center text-white">{data.handle}</p>
+
+              <div className="mt-20 flex flex-col gap-5">SortableContent</div>
+            </div>
+          </div>
+        </main>
+      </div>
+      <ToastContainer position="top-right" />
     </>
   );
-};
-
-export default Devtree;
+}
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,15 +1,21 @@
 import { Navigate } from "react-router-dom";
 import Devtree from "../components/Devtree";
+import type { User } from "../types";
 
 const AppLayout = () => {
   // first check if user authed
   const isLoading = false;
   const isError = false;
-  const data = {};
+  const data = {
+    handle: "roro",
+    name: "ronaldo",
+    email: "ronaldosalazar45@test.com",
+    _id: "1",
+  } as User;
 
   if (isLoading) return "Cargando...";
   if (isError) return <Navigate to={"/auth/login"} />;
-  if (data) return <Devtree />;
+  if (data) return <Devtree data={data} />;
 };
 
 export default AppLayout;
