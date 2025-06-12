a7191f1445cf1128a46ee90bf071872d351565df
Ronaldodev03 <ronaldosalazar.dev@gmail.com>
2025-06-12 15:59:38 -0400

    Add disabled in logout

--- src/components/nav/AdminNavigation.tsx
+++ src/components/nav/AdminNavigation.tsx
@@ -1,17 +1,22 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
 
 export default function AdminNavigation() {
   const queryClient = useQueryClient();
 
-  const logout = () => {
-    localStorage.removeItem("AUTH_TOKEN");
-    queryClient.invalidateQueries({ queryKey: ["user"] });
-  };
+  const logoutMutation = useMutation({
+    mutationFn: async () => {
+      localStorage.removeItem("AUTH_TOKEN");
+      await queryClient.invalidateQueries({ queryKey: ["user"] });
+    },
+  });
 
   return (
     <button
-      className=" bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer"
-      onClick={logout}
+      disabled={logoutMutation.isPending}
+      className={`bg-lime-500 p-2 text-slate-800 uppercase font-black text-xs rounded-lg cursor-pointer ${
+        logoutMutation.isPending ? "opacity-50" : "opacity-100"
+      }`}
+      onClick={() => logoutMutation.mutate()}
     >
       Cerrar Sesión
     </button>
